6ccd40d0ad6cdfdb823326288d49fe24eed6f0e4
kwangjae5033 <kwangjae5033@gmail.com>
2022-04-13 09:45:46 +0900

    multicheck box

--- app.py
+++ app.py
@@ -1,39 +1,18 @@
 import streamlit as st
-from datetime import time, datetime
 
-st.header('st.slider 에 대해서 알아보겠습니다')
+st.header('st.checkbox')
 
-# Example 1
+st.write ('What would you like to order?')
 
-st.subheader('Slider')
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+if icecream:
+     st.write("Great! Here's some more 🍦")
 
-# Example 2
+if coffee:
+     st.write("Okay, here's some coffee ☕")
 
-st.subheader('Range slider')
-
-values = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
-
-# Example 3
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 30), time(12, 45)))
-st.write("You're scheduled for:", appointment)
-
-# Example 4
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
+if cola:
+     st.write("Here you go 🥤")
