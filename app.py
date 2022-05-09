a772fe8ed7a3055f92dc8247edba3406951b155a
KwangJae Kim <kwangJae.Kim@novelis.com>
2022-05-09 09:46:47 +0900

    First Deployment

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-import streamlit as st
-import plotly.express
+import os
+# from multiprocessing import Process
 
+from PIL import Image
+
+import streamlit as st
+import plotly.express as px
+import plotly.figure_factory as ff
+
+import time
 import datetime
+from datetime import date
 import PIconnect as PI
+from pylogix import PLC
 
 import pandas as pd
 import numpy as np
@@ -10,10 +19,11 @@ import numpy as np
 import itertools
 
 import statsmodels.api as sm
-from statsmodels.tsa.stattools import adfullerㅁ
+from statsmodels.tsa.stattools import adfuller
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from scipy import stats
 
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
@@ -28,255 +38,118 @@ import missingno as msno
 import warnings
 warnings.filterwarnings('ignore')
 
-##############################################################
 
-## Check Server Status
-
-def find_server():
-    with PI.PIServer() as server:
-        return server.server_name
-
-
-print('server name:', find_server())
-
-## Data Transform
-
-def data_to_python(tag_list, start, end, freq):
-    with PI.PIServer() as server:
-        data_all = pd.DataFrame()
-
-        for tag in tag_list:
-            points = server.search(tag)[0]
-            data = points.interpolated_values(start_time=start, end_time=end, interval=freq)
-            data = pd.to_numeric(data, errors='coerce')
-            data_all = pd.concat([data_all, data], axis=1)
-    return data_all
-
-tag_list = ['Delac_1 Conveyor_Feedrate_PV'
-,'Delac_1 Kiln_Discharge_O2'
-,'Delac_1 WTCT4'
-,'Delac_1 WTCT3'
-,'Delac_1 Kiln_Gas_Inlet_Temp'
-,'Delac_1 Recirc_Fan_Motor_Requested_Speed'
-,'Delac_1 Diverter_Valve_Control_Valve_Feedback'
-,'Delac_1 Afterburner_O2'
-,'Delac_1 Afterburner_Temp'
-,'Delac_1 Afterburner_Temperature_Setpoint'
-,'Delac_1 Excess_Air_Valve_Motor_Control_Signal'
-,'Delac_1 Recirculation_Fan_Inlet_Temp'
-,'Delac_1 Kiln_Excess_Air_Valve_Feedback'
-,'Delac_1 Constant_Ideal_Kiln_Temp_Setpoint'
-,'Delac_1 WaterFlow_Kiln'
-,'Delac_1 WaterFlow_Afterburner'
-,'Delac_1 WaterFlow_Duct']
-
-PI.PIConfig.DEFAULT_TIMEZONE = 'Asia/Seoul'
-
-df = data_to_python(tag_list, '2022-04-01', '2022-04-30', '1m')
-df.index = pd.to_datetime(df.index).tz_localize(None)
-
-df['Z4_Setpoint_Delta'] = df['Delac_1 Constant_Ideal_Kiln_Temp_Setpoint'] - df['Delac_1 WTCT4']
-
-# MON : 0, Tue : 1, Wed : 2, Thurs : 3, Fri : 4, Sat : 5, Sun : 6
-df['dayofweek'] = df.index.dayofweek
-df = df[(df['dayofweek'] != 1)]
-
-df['Delac_1 WTCT3'].describe(percentiles=[0.05, 0.95])
-df['normal'] = np.where(df['Delac_1 WTCT3'].between(389.622300, 427.017200), 1, 0)
-df.resample('D')['normal'].mean().sort_values(ascending=False).head(10)
-
-s1 = '2022-04-03'
-s2 = '2022-04-04'
-s3 = '2022-04-06'
-s4 = '2022-04-10'
-s5 = '2022-04-13'
-s6 = '2022-04-14'
-s7 = '2022-04-18'
-s8 = '2022-04-21'
-s9 = '2022-04-28'
-s10 = '2022-04-30'
-
-mdl_data = pd.concat([df.loc[s1], df.loc[s2], df.loc[s3], df.loc[s4], df.loc[s5], df.loc[s6], df.loc[s7], df.loc[s8], df.loc[s9], df.loc[s10]], axis=0)
-
-var_list = ['Delac_1 WTCT4', 'Delac_1 WTCT3', 'Delac_1 Recirc_Fan_Motor_Requested_Speed', 'Delac_1 Diverter_Valve_Control_Valve_Feedback', 'Delac_1 Conveyor_Feedrate_PV', 'Delac_1 Kiln_Discharge_O2', 'Delac_1 Kiln_Gas_Inlet_Temp', 'Delac_1 Afterburner_O2', 'Delac_1 Afterburner_Temp', 'Delac_1 Kiln_Excess_Air_Valve_Feedback', 'Z4_Setpoint_Delta']
-mdl_data = mdl_data[var_list].copy()
-
-for var in var_list:
-    mdl_data[var + '_dif1'] = mdl_data[var] - mdl_data[var].shift(1)
-    mdl_data[var + '_dif1_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(2)
-    mdl_data[var + '_dif1_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(3)
-    mdl_data[var + '_dif1_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(4)
-    mdl_data[var + '_dif1_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(5)
-    mdl_data[var + '_dif1_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(6)
-    mdl_data[var + '_dif1_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(7)
-    mdl_data[var + '_dif1_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(8)
-    mdl_data[var + '_dif1_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif1_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(10)
-
-    mdl_data[var + '_dif2'] = mdl_data[var] - mdl_data[var].shift(2)
-    mdl_data[var + '_dif2_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(3)
-    mdl_data[var + '_dif2_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(4)
-    mdl_data[var + '_dif2_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(5)
-    mdl_data[var + '_dif2_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(6)
-    mdl_data[var + '_dif2_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(7)
-    mdl_data[var + '_dif2_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(8)
-    mdl_data[var + '_dif2_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif2_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif2_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(11)
-
-    mdl_data[var + '_dif3'] = mdl_data[var] - mdl_data[var].shift(3)
-    mdl_data[var + '_dif3_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(4)
-    mdl_data[var + '_dif3_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(5)
-    mdl_data[var + '_dif3_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(6)
-    mdl_data[var + '_dif3_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(7)
-    mdl_data[var + '_dif3_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(8)
-    mdl_data[var + '_dif3_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif3_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif3_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(11)
-    mdl_data[var + '_dif3_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(12)
-
-    mdl_data[var + '_dif4'] = mdl_data[var] - mdl_data[var].shift(4)
-    mdl_data[var + '_dif4_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(5)
-    mdl_data[var + '_dif4_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(6)
-    mdl_data[var + '_dif4_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(7)
-    mdl_data[var + '_dif4_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(8)
-    mdl_data[var + '_dif4_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif4_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif4_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(11)
-    mdl_data[var + '_dif4_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(12)
-    mdl_data[var + '_dif4_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(13)
-
-    mdl_data[var + '_dif5'] = mdl_data[var] - mdl_data[var].shift(5)
-    mdl_data[var + '_dif5_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(6)
-    mdl_data[var + '_dif5_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(7)
-    mdl_data[var + '_dif5_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(8)
-    mdl_data[var + '_dif5_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif5_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif5_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(11)
-    mdl_data[var + '_dif5_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(12)
-    mdl_data[var + '_dif5_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(13)
-    mdl_data[var + '_dif5_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(14)
-
-    mdl_data[var + '_dif6'] = mdl_data[var] - mdl_data[var].shift(6)
-    mdl_data[var + '_dif6_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(7)
-    mdl_data[var + '_dif6_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(8)
-    mdl_data[var + '_dif6_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif6_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif6_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(11)
-    mdl_data[var + '_dif6_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(12)
-    mdl_data[var + '_dif6_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(13)
-    mdl_data[var + '_dif6_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(14)
-    mdl_data[var + '_dif6_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(15)
-
-    mdl_data[var + '_dif7'] = mdl_data[var] - mdl_data[var].shift(7)
-    mdl_data[var + '_dif7_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(8)
-    mdl_data[var + '_dif7_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif7_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif7_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(11)
-    mdl_data[var + '_dif7_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(12)
-    mdl_data[var + '_dif7_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(13)
-    mdl_data[var + '_dif7_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(14)
-    mdl_data[var + '_dif7_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(15)
-    mdl_data[var + '_dif7_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(16)
-
-    mdl_data[var + '_dif8'] = mdl_data[var] - mdl_data[var].shift(8)
-    mdl_data[var + '_dif8_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(9)
-    mdl_data[var + '_dif8_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif8_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(11)
-    mdl_data[var + '_dif8_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(12)
-    mdl_data[var + '_dif8_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(13)
-    mdl_data[var + '_dif8_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(14)
-    mdl_data[var + '_dif8_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(15)
-    mdl_data[var + '_dif8_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(16)
-    mdl_data[var + '_dif8_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(17)
-
-    mdl_data[var + '_dif9'] = mdl_data[var] - mdl_data[var].shift(9)
-    mdl_data[var + '_dif9_shift1'] = mdl_data[var].shift(1) - mdl_data[var].shift(10)
-    mdl_data[var + '_dif9_shift2'] = mdl_data[var].shift(2) - mdl_data[var].shift(11)
-    mdl_data[var + '_dif9_shift3'] = mdl_data[var].shift(3) - mdl_data[var].shift(12)
-    mdl_data[var + '_dif9_shift4'] = mdl_data[var].shift(4) - mdl_data[var].shift(13)
-    mdl_data[var + '_dif9_shift5'] = mdl_data[var].shift(5) - mdl_data[var].shift(14)
-    mdl_data[var + '_dif9_shift6'] = mdl_data[var].shift(6) - mdl_data[var].shift(15)
-    mdl_data[var + '_dif9_shift7'] = mdl_data[var].shift(7) - mdl_data[var].shift(16)
-    mdl_data[var + '_dif9_shift8'] = mdl_data[var].shift(8) - mdl_data[var].shift(17)
-    mdl_data[var + '_dif9_shift9'] = mdl_data[var].shift(9) - mdl_data[var].shift(18)
-
-
-mdl_data['target_5'] = mdl_data['Delac_1 WTCT3'].shift(-5) - mdl_data['Delac_1 WTCT3']
-
-mdl_data.drop(columns=var_list, inplace=True)
-
-mdl_data = mdl_data.dropna()
-
-mdl_data['Delac_1 WTCT3_dif1'].describe(percentiles=[0.05, 0.95])
-mdl_data['normal'] = np.where(mdl_data['Delac_1 WTCT3_dif1'].between(-2.498835, 2.305695), 1, 0)
-mdl_data.resample('D')['normal'].mean().sort_values(ascending=False)
-
-s1 = '2022-04-03'
-s2 = '2022-04-04'
-s3 = '2022-04-06'
-s4 = '2022-04-10'
-s5 = '2022-04-13'
-s6 = '2022-04-14'
-s7 = '2022-04-18'
-s8 = '2022-04-21'
-s9 = '2022-04-28'
-
-mdl_data = pd.concat([mdl_data.loc[s1], mdl_data.loc[s2], mdl_data.loc[s3], mdl_data.loc[s4], mdl_data.loc[s5], mdl_data.loc[s6], mdl_data.loc[s7], mdl_data.loc[s8], mdl_data.loc[s9]], axis=0)
-
-mdl_data = mdl_data.dropna()a
-
-mdl_data.to_pickle("data.pkl")
-
-##############################################################
-
+####################################################################################
+## Start of the page
+novelis_logo = Image.open("novelis.png")
 st.set_page_config(
-    page_title = 'Real-Time Zone3 Temp Prediction Dashboard',
+    page_title = 'Reduction Black Dross / Zone3 Temp Prediction Dashboard',
+    page_icon = novelis_logo,
     layout = 'wide'
 )
 
-
 st.title('Real-Time / Z3 Temperature Prediction Dashboard')
+
+st.text(" ")
+st.text(" ")
+st.text(" ")
+st.text(" ")
+st.text(" ")
+st.text(" ")
+
+# Load z3 Data for distplot
+z3_distplot = pd.read_pickle('z3_distplot.pkl')
 
 # creating a single-element container
 placeholder = st.empty()
 
+with PLC() as Delac_1_comm, PLC() as Delac_2_comm:
+
+    Delac_1_comm.IPAddress = '10.44.192.50' # Decoater 1
+    Delac_1_comm.SetPLCTime()
+    Delac_2_comm.IPAddress = '10.44.192.70' # Decoater 2
+    Delac_2_comm.SetPLCTime()
+
+    Delac_1_plc_df = pd.DataFrame()
+    Delac_2_plc_df = pd.DataFrame()
+
+    while True:
+        ###############################################
+        time_now = pd.Series(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), name='plc_time')
+        Delac_1_z3_temperature = pd.Series(Delac_1_comm.Read('Kiln_Temperature_Zone_3').Value, name='Delac_1 z3', dtype='float')
+        Delac_2_z3_temperature = pd.Series(Delac_2_comm.Read('Kiln_Temperature_Zone_3').Value, name='Delac_2 z3', dtype='float')
+        Delac_1_z3_pred = pd.Series(Delac_1_comm.Read('Kiln_Temperature_Zone_3_Prediction').Value, name='Delac_1 z3 Pred', dtype='float')
+        Delac_2_z3_pred = pd.Series(Delac_2_comm.Read('Kiln_Temperature_Zone_3_Prediction').Value, name='Delac_2 z3 Pred', dtype='float')
+
+        Delac_1_plc_temp = pd.concat([time_now, Delac_1_z3_temperature, Delac_1_z3_pred], axis=1)
+        Delac_2_plc_temp = pd.concat([time_now, Delac_2_z3_temperature, Delac_2_z3_pred], axis=1)
+
+        Delac_1_plc_df = pd.concat([Delac_1_plc_df, Delac_1_plc_temp], axis=0)
+        Delac_2_plc_df = pd.concat([Delac_2_plc_df, Delac_2_plc_temp], axis=0)
+
+        Delac_1_plc_df = Delac_1_plc_df.tail(1500)
+        Delac_2_plc_df = Delac_2_plc_df.tail(1500)
+        ###############################################
+
+        with placeholder.container():
+
+            # Current Zone3 Temp and Prediction
+            Delac_1_z3_now, Delac_2_z3_now = st.columns(2)
+            st.text(" ")
+            st.text(" ")
+            Delac_1_z3_hat, Delac_2_z3_hat = st.columns(2)
+            st.text(" ")
+            st.text(" ")
+
+            Delac_1_z3_now.metric(label='Current Delac1 Zone3 Temp 🔥', value=round(Delac_1_plc_df['Delac_1 z3'].iloc[-1], 1)) # delta=round(Delac_1_plc_df['Delac_1 z3'].iloc[-1] - Delac_1_plc_df['Delac_1 z3'].iloc[-2]
+            Delac_1_z3_hat.metric(label='Pred Delac1 Zone3 Temp 📈', value=round(Delac_1_plc_df['Delac_1 z3 Pred'].iloc[-1], 1)) # delta=round(Delac_1_plc_df['Delac_1 z3 Pred'].iloc[-1] - Delac_1_plc_df['Delac_1 z3 Pred'].iloc[-2]
+            Delac_2_z3_now.metric(label='Current Delac2 Zone3 Temp 🔥', value=round(Delac_2_plc_df['Delac_2 z3'].iloc[-1], 1)) # delta=round(Delac_2_plc_df['Delac_2 z3'].iloc[-1] - Delac_2_plc_df['Delac_2 z3'].iloc[-2]
+            Delac_2_z3_hat.metric(label='Pred Delac2 Zone3 Temp 📈', value=round(Delac_2_plc_df['Delac_2 z3 Pred'].iloc[-1], 1)) # delta=round(Delac_2_plc_df['Delac_2 z3 Pred'].iloc[-1] - Delac_2_plc_df['Delac_2 z3 Pred'].iloc[-2]
+
+            # create two columns for charts
+            st.text(" ")
+            st.text(" ")
+            st.text(" ")
+            st.text(" ")
+            st.text(" ")
+            st.text(" ")
+
+            Delac_1_true_vs_predict_chart, Delac_2_true_vs_predict_chart = st.columns(2)
+
+            with Delac_1_true_vs_predict_chart:
+                st.markdown("### Comparison on Delac_1 True vs Predict")
+                fig = px.line(Delac_1_plc_df, x='plc_time', y=['Delac_1 z3', 'Delac_1 z3 Pred'], color_discrete_map={'Delac_1 z3':'blue', 'Delac_1 z3 Pred':'red'})
+                st.write(fig)
+
+            with Delac_2_true_vs_predict_chart:
+                st.markdown("### Comparison on Delac_2 True vs Predict")
+                fig = px.line(Delac_2_plc_df, x='plc_time', y=['Delac_2 z3', 'Delac_2 z3 Pred'], color_discrete_map={'Delac_2 z3':'blue', 'Delac_2 z3 Pred':'red'})
+                st.write(fig)
+
+            Delac_1_standard_deviation_chart, Delac_2_standard_deviation_chart = st.columns(2)
+
+            with Delac_1_standard_deviation_chart:
+                st.markdown("### Comparison on   Delac_1   Standard deviation")
+                hist_data = [z3_distplot['Delac_1 WTCT3'], z3_distplot['Delac_2 WTCT3']]
+                group_labels = ['Delac_1 z3 Std', 'Delac_2 z3 Std']
+                colors = ['#2BCDC1', '#F66095']
+                # Create distplot with curve_type set to 'kde'
+                fig = ff.create_distplot(hist_data, group_labels, show_hist=False, colors=colors, curve_type='kde')
+                st.write(fig)
+
+            with Delac_2_standard_deviation_chart:
+                st.markdown("### Comparison on   Delac_2   Standard deviation")
+                hist_data = [z3_distplot['Delac_1 WTCT3'], z3_distplot['Delac_2 WTCT3']]
+                group_labels = ['Delac_1 z3 Std', 'Delac_2 z3 Std']
+                colors = ['#2BCDC1', '#F66095']
+                # Create distplot with curve_type set to 'kde'
+                fig = ff.create_distplot(hist_data, group_labels, show_hist=False, colors=colors, curve_type='kde')
+                st.write(fig)
+
+        time.sleep(60 - time.time() % 60)
+        print()
+##############################################################
+
+
 
 ##############################################################
-# while True:
-#
-#     with placeholder.container():
-#         # create three columns for KPIS
-#         kpi_1, kpi_2, kpi_3 = st.columns(3)
-#
-#         # fill in those three columns with respective metrics or KPIS
-#         kpi_1.metric(label='Zone3 Temp / Delac1', value= , delta=, delta_color= )
-#
-#
-#         # create two columns for charts
-#         fig_col_1, fig_col_2 = st.columns(2)
-#         with fig_col_1:
-#             st.markdown("### First Chart")
-#             fig = px.
-#             st.write(fig)
-
-##############################################################
-
-
-
-st.header('st.checkbox')
-
-st.write ('What would you like to order?')
-
-icecream = st.checkbox('Ice cream')
-coffee = st.checkbox('Coffee')
-cola = st.checkbox('Cola')
-
-if icecream:
-     st.write("Great! Here's some more 🍦")
-
-if coffee:
-     st.write("Okay, here's some coffee ☕")
-
-if cola:
-     st.write("Here you go 🥤")
